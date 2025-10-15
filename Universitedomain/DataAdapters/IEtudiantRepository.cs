1cb292e406ca41dbb2c421c18e286bef3e244858
Lamia Ait Mimoune <lamia.ait.mimoune@etud.u-picardie.fr>
2025-10-15 15:30:19 +0200

    commit and push seconde

---
+++ Universitedomain/DataAdapters/IEtudiantRepository.cs
@@ -0,0 +1,14 @@
+namespace Universitedomain.DataAdapters;
+
+public interface IEtudiantRepository
+{
+    Task<Etudiant> CreateAsync(Etudiant entity);
+    Task UpdateAsync(Etudiant entity);
+    Task DeleteAsync(long id);
+    Task DeleteAsync(Etudiant entity);
+    Task<Etudiant?> FindAsync(long id);
+    Task<Etudiant?> FindAsync(params object[] keyValues);
+    Task<List<Etudiant>> FindByConditionAsync(Expression<Func<Etudiant, bool>> condition);
+    Task<List<Etudiant>> FindAllAsync();
+    Task SaveChangesAsync();
+}
